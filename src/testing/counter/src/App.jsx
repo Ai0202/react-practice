eee0c09e0ba7602ee478d886a70c0171daf17250
Atsushi <atsushi88885@gmail.com>
2020-06-13 17:11:17 +0900

    counter up create some test

---
+++ src/testing/counter/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    }
+  }
+
+  render() {
+    return (
+      // id や class属性が存在するかテストで判断可能だが、
+      // 変更される可能性があるため、
+      // test確認用の属性を付与してる
+      <div data-test="component-app">
+        <h1 data-test="counter-display">
+          The coutner is currently { this.state.counter }
+        </h1>
+        <button 
+          data-test="increment-button"
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
+          Increment Counter
+        </button>
+        <button 
+          data-test="decrement-button"
+          onClick={() => this.setState({ counter: this.state.counter - 1 })}
+        >
+          Decrement Counter
+        </button>
+      </div>
+    )
+  }
+}
+
+export default App;
--- src/testing/counter/src/App.test.js
+++ src/testing/counter/src/App.test.js
@@ -1,30 +1,91 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 import App from './App';
 
-Enzyme.configure( { adapter: new EnzymeAdapter() })
+Enzyme.configure( { adapter: new EnzymeAdapter() });
+
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props 
+ * @param {any} state 
+ * @retruns {ShallowWrapper}
+ */
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />)
+
+  if (state) wrapper.setState(state);
+
+  return wrapper;
+}
+
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ * @param {ShallowWrapper} wrapper 
+ * @param {string} val 
+ * @returns {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
 
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app')
 
   expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button')
 
+  expect(button.length).toBe(1);
 });
 
 test('renders ccounter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display')
 
+  expect(counterDisplay.length).toBe(1);
 });
 
 test('counter starts at 0', () => {
-  
+  // setup関数でApp classがかえる?
+  const wrapper = setup();
+
+  // app classのstateを変数に代入
+  const initialCounterState = wrapper.state('counter');
+
+  // 変数が0であるか確認する
+  expect(initialCounterState).toBe(0);
 });
 
 test('clicking button increments counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
   
+  // find button and click
+  const button = findByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+
+  // find display and test value
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+  expect(counterDisplay.text()).toContain(counter + 1);
+
+});
+
+test('clicking button decrements counter display', () => {
+  const counter = 10;
+  const wrapper = setup(null, { counter });
+
+  // find button and click
+  const button = findByTestAttr(wrapper, 'decrement-button');
+  button.simulate('click');
+
+  // find display and test value;
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter - 1);
 });
