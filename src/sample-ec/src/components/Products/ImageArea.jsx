774c3add2c6360fc248b0f1acb030e3febbef246
Atsushi <atsushi88885@gmail.com>
2020-07-25 12:30:20 +0900

    画像削除処理

--- src/sample-ec/src/components/Products/ImageArea.jsx
+++ src/sample-ec/src/components/Products/ImageArea.jsx
@@ -46,11 +46,23 @@ const ImageArea = props => {
     }, [props.setImages])
   })
 
+  const deleteImage = useCallback(async (id) => {
+    const ret = window.confirm('この画像を削除しますか？')
+
+    if (!ret) {
+      return false
+    }
+
+    const newImages = images.filter(image => image.id !== id)
+    props.setImages(newImages)
+    return storage.ref('images').child(id).delete()
+  }, [images])
+
   return (
     <div>
       <div className="p-grid__list-images">
         {images.length > 0 && (
-          images.map(image => <ImagePreview id={image.id} path={image.path} key={image.id} />)
+          images.map(image => <ImagePreview id={image.id} path={image.path} key={image.id} delete={deleteImage} />)
         )}
       </div>
       <div className="u-text-right">
--- src/sample-ec/src/components/Products/ImagePreview.jsx
+++ src/sample-ec/src/components/Products/ImagePreview.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ImagePreview = props => {
   return (
-    <div className="p-media__thumb" onClick={() => {}}>
+    <div className="p-media__thumb" onClick={() => props.delete(props.id)}>
       <img src={props.path} alt="アイキャッチ画像" />
     </div>
   )
