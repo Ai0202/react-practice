669b32a6e1c13dbf6de5f57985495d88d7fb4458
Atsushi <atsushi88885@gmail.com>
2020-08-02 14:29:49 +0900

    注文完了画面作成

--- src/sample-ec/src/Router.js
+++ src/sample-ec/src/Router.js
@@ -8,7 +8,8 @@ import {
   ProductEdit,
   ProductList, 
   ProductDetail,
-  OrderConfirm} from "./templates"
+  OrderConfirm,
+  OrderComplete} from "./templates"
 import Auth from "./Auth"
 
 const Router = () => {
@@ -22,6 +23,7 @@ const Router = () => {
         <Route exact path="/" component={ProductList} />
         <Route exact path="/cart" component={CartList} />
         <Route exact path="/orders/confirm" component={OrderConfirm} />
+        <Route exact path="/orders/complete" component={OrderComplete} />
         <Route exact path="/products/:id" component={ProductDetail} />
         <Route path="/products/edit(/:id)?" component={ProductEdit} />
       </Auth>
