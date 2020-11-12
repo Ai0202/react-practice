83339a53aed3fa53d53fb89bf706d7f8a1e46077
Atsushi <atsushi88885@gmail.com>
2020-11-12 23:51:54 +0900

    フッターの整理

--- src/material_ui_sample/src/components/Footer.tsx
+++ src/material_ui_sample/src/components/Footer.tsx
@@ -1,44 +1,51 @@
 import React from 'react'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
-import { Link } from '@material-ui/core'
-import Typography from '@material-ui/core/Typography'
+import { Grid, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       // flexGrow: 1,
       // backgroundColor: theme.palette.background.paper,
-      backgroundColor: '#ddd',
-      padding: theme.spacing(6),
+      // backgroundColor: '#ddd',
+      padding: theme.spacing(3),
     },
+    link: {
+      textDecoration: "none",
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+      color: theme.palette.primary.main,
+    },
+    list: {
+      listStyle: "none",
+      display: "flex",
+    }
   }),
 )
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Takuma Totani
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  )
-}
 
 export const Footer = () => {
   const classes = useStyles()
 
   return (
-    <footer className={classes.root}>
-      <Typography variant="h6" align="center" gutterBottom>
-        Emoi
+      <Typography className={classes.root} component="footer">
+        <Grid container justify="space-between" alignItems="center">
+          <Grid item sm={8}>
+            <Typography variant="h6">
+              Emoiロゴ
+            </Typography>
+          </Grid>
+          <Grid item sm={4}>
+            <Typography className={classes.list} component="ul">
+              <Typography component="li" variant="subtitle1">
+                <Link className={classes.link} to="/teams">利用規約</Link>
+              </Typography>
+              <Typography component="li" variant="subtitle1">
+                <Link className={classes.link} to="/privacy">プライバシーポリシー</Link>
+              </Typography>
+            </Typography>
+          </Grid>
+        </Grid>
       </Typography>
-      <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-        Something here to give the footer a purpose!
-      </Typography>
-      <Copyright />
-    </footer>
   )
 }
